400eaeca8e5c273a7740677141b2d648ebb231d9
Saptarshi-1 <saptarshydas9@gmail.com>
2025-07-17 23:31:48 +0530

    netlify added

---
+++ remix.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+    serverBuildTarget: "netlify",
+    server: "./server.js", // or "./server.ts" if you use TypeScript
+    // ...other config
+  };

9a180669209adb7f92ce8fd9f22757bebc1163f0
Saptarshi-1 <saptarshydas9@gmail.com>
2025-07-17 23:52:52 +0530

    remove the server js code written in remix config

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
     serverBuildTarget: "netlify",
-    server: "./server.js", // or "./server.ts" if you use TypeScript
     // ...other config
   };
